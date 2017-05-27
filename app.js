c461cc42c86b54dc036c538c7fcc0d2a9bda3a66
Niall Wallace <nj.wallace@outlook.com>
2017-05-27 16:42:34 +0100

    added seneca component

--- app.js
+++ app.js
@@ -11,4 +11,18 @@ seneca.add({ role: 'math', cmd: 'prod'}, function (msg, res) {
   res(null, prod)
 });
 
-seneca.act({ role: 'math', cmd: 'sum', left: 3, right: 10}, console.log);
+seneca.add({ component: 'greeter' }, function (msg, res) {
+  res(null, { message: 'Hello! ' + msg.name });
+}); 
+
+seneca.act({ role: 'math', cmd: 'sum', left: 3, right: 10}, function (err, data) {
+  if (err) {
+    return console.error(err)
+  } 
+  console.log(data)
+});
+
+seneca.act({ component: 'greeter', name: 'Niall'}, function (err, res) {
+  if (err) return console.error(err);
+  console.log(res.message);
+});
